b1bc4bd3aa84ba248351adb5a48e152e03e19521
vincent-the-gamer <1127620453@qq.com>
2024-01-25 22:00:47 +0800

    feat: add search.

--- contents/.vitepress/configs/zh_hans.ts
+++ contents/.vitepress/configs/zh_hans.ts
@@ -4,29 +4,6 @@ import getNavs from "../navs/zh_hans";
 
 export const zhHansConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
     themeConfig: {
-        search: {
-            provider: "local",
-            options: {
-                locales: {
-                    zh: {
-                        translations: {
-                            button: {
-                                buttonText: '搜索文档',
-                                buttonAriaLabel: '搜索文档'
-                            },
-                            modal: {
-                                noResultsText: '无法找到相关结果',
-                                resetButtonTitle: '清除查询条件',
-                                footer: {
-                                    selectText: '选择',
-                                    navigateText: '切换'
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        },
         logo: "/logo/logo.png",
         outlineTitle: "目录",
         outline: "deep",
